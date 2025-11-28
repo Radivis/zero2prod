fd1aec2a8f3a54917f4b0fa4c465b30c9b8e48da
Radivis <radivis@radivis.com>
2025-11-28 14:42:13 +0100

    Added POST /subscriptions endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
 use std::net::TcpListener;
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
 async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+// Let's start simple: we always return a 200 OK
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 // We need to mark `run` as public.
@@ -11,6 +22,9 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
+
     })
         .listen(listener)?
         .run();
