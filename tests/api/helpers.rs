6a8908da21258bbc4042345cb48adf54bc5fecd3
Radivis <radivis@radivis.com>
2025-12-09 11:11:54 +0100

    Suppress let_underscore_future warning in spawn_app

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -51,6 +51,7 @@ pub async fn spawn_app() -> TestApp {
         .expect("Failed to build application.");
     let address = format!("http://127.0.0.1:{}", application.port());
 
+    #[allow(clippy::let_underscore_future)]
     let _ = tokio::spawn(application.run_until_stopped());
 
     TestApp {
