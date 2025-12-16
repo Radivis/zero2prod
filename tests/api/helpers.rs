a6ecf15be6ba6aeb2e749b68860dea700690ea9a
Radivis <radivis@radivis.com>
2025-12-16 14:40:11 +0100

    Redirect users without valid session to login page

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -83,15 +83,16 @@ pub struct TestApp {
 impl TestApp {
     // Our tests will only look at the HTML page, therefore
     // we do not expose the underlying reqwest::Response
-    pub async fn get_admin_dashboard_html(&self) -> String {
+
+    pub async fn get_admin_dashboard(&self) -> reqwest::Response {
         self.api_client
             .get(format!("{}/admin/dashboard", &self.address))
             .send()
             .await
             .expect("Failed to execute request.")
-            .text()
-            .await
-            .unwrap()
+    }
+    pub async fn get_admin_dashboard_html(&self) -> String {
+        self.get_admin_dashboard().await.text().await.unwrap()
     }
 
     pub async fn get_login_html(&self) -> String {
